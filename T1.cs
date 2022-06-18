63305e1892e22e981b138d2ff979de6544c5f655
EmoryC <emory-crane@live.nmit.ac.nz>
2022-06-18 20:50:09 +1200

    guidelines

---
+++ T1.cs
@@ -0,0 +1,8 @@
+/*
+Task One - Calculator:
+You will create a set of Math operators (+, -, /, *, pow and remainder) by getting the TWO input numbers
+from the user and printing out the result on the command windows in MS Visual Studio 2022 community,
+each Math operator result must be printed in a different background and foreground color.
+
+Not using Studio bc it is new,confuses me, and I find it unecessary since I'm solid with VS Code
+*/
