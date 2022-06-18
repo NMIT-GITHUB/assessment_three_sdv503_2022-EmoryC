c90a3cfd54e0c23ddef18d2789f1e9c73695026a
EmoryC <emory-crane@live.nmit.ac.nz>
2022-06-18 20:24:03 +1200

    setup

--- T2.cs
+++ T2.cs
@@ -17,3 +17,15 @@ function exampleJavaScript(arr) {
 console.log(exampleJavaScript([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]])) // outcome [7, 90, 2]
 
 */
+class Convert //Instancing the class "Convert"
+{
+    static void Arr() //setting up the function strictly within Convert class
+    {
+         int[,] NewArray; //The input data being pushed through the function, setup as a multidimentional array of  arrays
+        {
+            new int[,] {{4, 2, 7, 1}, {20, 70, 40, 90}, {1, 2, 0}};
+        };
+
+        
+    }
+};
