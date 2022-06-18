6b5c1cb66181f4f7f8d27336c405430a1511e22e
EmoryC <emory-crane@live.nmit.ac.nz>
2022-06-18 20:13:49 +1200

    T2

---
+++ T2.cs
@@ -0,0 +1,19 @@
+/*
+
+function exampleJavaScript(arr) {
+	let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    let largest = arr[i][0];
+    for(let j = 0; j < arr[i].length; j++){
+      if(arr[i][j] > largest) {
+        largest = arr[i][j]
+      }
+    }
+    newArr.push(largest)
+  }
+  return newArr
+}
+
+console.log(exampleJavaScript([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]])) // outcome [7, 90, 2]
+
+*/
