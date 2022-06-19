08b0aa9bbd24901d1fe4e021af7b8fec425d6342
EmoryC <emory-crane@live.nmit.ac.nz>
2022-06-19 17:18:06 +1200

    t1

--- T1.cs
+++ T1.cs
@@ -4,5 +4,20 @@ You will create a set of Math operators (+, -, /, *, pow and remainder) by getti
 from the user and printing out the result on the command windows in MS Visual Studio 2022 community,
 each Math operator result must be printed in a different background and foreground color.
 
-Not using Studio bc it is new,confuses me, and I find it unecessary since I'm solid with VS Code
+Not using Studio bc it is new, confuses me, and I find it unecessary since I'm solid with VS Code
+That beind said I never had node.js installed on this system so there will be no testing, just praying
 */
+
+class Calculator
+{
+
+    static void Int()
+    {
+
+        
+    }
+
+
+
+}
+
